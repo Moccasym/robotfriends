1a78e7bfe2396937fba2afc43a4060c1236f494f
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-02-21 15:01:19 +0100

    merged master and branch CreatingComponents

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import robots from './robots';
-import Card from './Card';
+// import robots from './robots';
+// import Card from './Card';
+import CardList from './CardList'
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 import { StrictMode } from 'react';
@@ -12,11 +13,7 @@ import { StrictMode } from 'react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-    
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    
+        <CardList />
     </StrictMode>
 );
 
